2e327c6282b5763e0cf123d2b097bdddbfb78d69
KurzGedanke <KurzGedanke@protonmail.com>
2019-01-04 00:09:53 +0100

    first functanality

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A mastodon bot, which will toot every 12 hours the current day.
 
 ## Usage
 
-Create a secret.py which looks like this and fill it out with your data. This is done so that credentials stay out of git.
+Create a 'cred.py' which looks like this and fill it out with your data. This is done so that credentials stay out of git.
 
 ```python
 email = ''
--- whatTheDay.py
+++ whatTheDay.py
@@ -1,18 +1,31 @@
 from mastodon import Mastodon
+import schedule
 import cred
+import time
 
+
+
+def post():
+    weekday = time.strftime('%A')
+    dateDMY =time.strftime('%d/%m/%Y')
+    clock = time.strftime("%X")
+
+    mastodon = Mastodon(
+        api_base_url = 'https://botsin.space/',
+        access_token = cred.accessToken
+    )
+    mastodon.log_in(
+        cred.email,
+        cred.password,
+    )
+    mastodon.status_post(f'Today is {weekday}, the {dateDMY} and it is {clock} o\'clock!', visibility='unlisted')
 
 def main():
-    # mastodon = Mastodon(
-    #     client_id = 'pytooter_clientcred.secret',
-    #     api_base_url = ''
-    # )
-    # mastodon.log_in(
-    #     '',
-    #     '',
-    #     to_file = ''
-    # )
-    print(cred.email)
+    schedule.every(2).seconds.do(post)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 
 
 
